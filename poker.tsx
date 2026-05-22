84e2850114fdf3e66faf1f0857c0efd03dface8b
xeyalamanov121-commits <xeyalamanov121@gmail.com>
2026-05-22 16:11:31 +0400

    Create poker.tsx

---
+++ poker.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Alert, TextInput } from 'react-native';
+import { io } from 'socket.io-client';
+
+// HuggingFace linkinizi bura yazın:
+const POKER_URL = 'https://xayal12-poker.hf.space'; 
+
+export default function PokerScreen() {
+  // ... (poker.tsx faylınızdakı bütün kodları buraya yapışdırın)
+  // Faylın tam məzmunu yuxarıda təqdim edilmişdir.
+}
+
